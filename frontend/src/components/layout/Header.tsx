1d7c82c6331897a0f3c48aec60c64f94170d4d90
dak-1306 <thd13062005@gmail.com>
2026-04-09 22:54:09 +0700

    fix: update import path for useAuth in Header component

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../../components/ui/button";
 import { Link } from "react-router-dom";
-import { useAuth } from "../../features/auth/context/useAuth";
+import { useAuth } from "../../features/auth/context/AuthContext";
 function Header() {
   const { isLogin } = useAuth();
   return (
